93261b6b23fd5498841f7cb40906ebdacb19f885
Steve Law <slaw@unearthlyentertainment.com>
2015-09-16 11:01:02 -0400

    initial commit

---
+++ Ncarb/Models/NCarbData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ncarb.Models
+{
+    public class NCarbData
+    {
+        string id { get; set; }
+        string firstName { get; set; }
+        string lastName { get; set; }
+        string city { get; set; }
+        string stateCode { get; set; }
+        string countryCode { get; set; }
+    }
+}
---
+++ Ncarb/Scripts/app/NCarb.js
@@ -0,0 +1,40 @@
+﻿angular.module('main')
+    .controller('testController', ['$scope', function ($scope) {
+    $scope.firstName= "John";
+    $scope.lastName= "Doe";
+}]);
+
+angular.module('main')
+    .controller('filterController', function ($scope, $http) {
+
+    //    $scope.names = "";
+
+        $scope.lname = "";
+        $scope.gridOptions = {
+            data: 'names'
+        };
+
+        $scope.filter = function () {
+            $http.get("https://my.ncarb.org/Public/api/certification/search?pageSize=20&page=0&lastName=" + $scope.lname + "&firstName=&city=&stateCode=&countryCode=&orderBy=name")
+    .success(function (response) {
+       // gridOptions.data.length = 0;
+       // angular.copy(response.data, $scope.names);
+       // $scope.gridOptions.data = response.data;
+
+        $scope.names = [];
+         $scope.names = response.data;
+        
+       // $scope.gridOptions.data = [];
+
+      //  $timeout(function () {
+         //   $scope.gridOptions.data = response.data;
+     //   });
+
+    });
+        };
+         $scope.filter();
+        
+
+
+    });
+
